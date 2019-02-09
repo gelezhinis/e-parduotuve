65aba7f65f8dcb556e50149d815f15416585c69a
gelezhinis <gelezhinis@gmail.com>
2019-02-09 14:33:40 +0200

    Updates

---
+++ precache-manifest.7b32f1502375bd4494deb0b4315f8584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/e-parduotuve/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "c70c2a8bbef6cb41c4b0",
+    "url": "/e-parduotuve/static/js/runtime~main.c70c2a8b.js"
+  },
+  {
+    "revision": "46df04b829923bed3ad1",
+    "url": "/e-parduotuve/static/js/main.46df04b8.chunk.js"
+  },
+  {
+    "revision": "445c696d9f2c4c29e406",
+    "url": "/e-parduotuve/static/js/1.445c696d.chunk.js"
+  },
+  {
+    "revision": "46df04b829923bed3ad1",
+    "url": "/e-parduotuve/static/css/main.b595f432.chunk.css"
+  },
+  {
+    "revision": "445c696d9f2c4c29e406",
+    "url": "/e-parduotuve/static/css/1.50f95f51.chunk.css"
+  },
+  {
+    "revision": "8447c9aa381c6d1cd25ae2e0f92d68da",
+    "url": "/e-parduotuve/index.html"
+  }
+];
